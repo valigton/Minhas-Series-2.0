ac94a79f77da6f55a7045d55e45b01a3ead67f0a
valigton <valigtonverissimo@outlook.com>
2020-06-06 21:11:23 -0300

    fix

--- src/app/popular-movies/popular-movies-api.service.ts
+++ src/app/popular-movies/popular-movies-api.service.ts
@@ -1,17 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
 })
 export class PopularMoviesApiService {
-  PUBLIC_KEY = '0268e9667082b43101efe70dc24d137b';
-  URL_API = `https://api.themoviedb.org/3/movie/popular?api_key=${this.PUBLIC_KEY}&language=pt-BR&page=1`;
+  
   constructor(private http: HttpClient) { }
-
-  getPopular(): Observable<any> {
-    return this.http.get<any>(this.URL_API)
-  }
+  
 }
--- src/app/popular-movies/popular-movies.component.ts
+++ src/app/popular-movies/popular-movies.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit  } from '@angular/core';
-import { Observable } from 'rxjs';
 import { PopularMoviesApiService } from './popular-movies-api.service';
 
 @Component({
@@ -8,13 +7,23 @@ import { PopularMoviesApiService } from './popular-movies-api.service';
   styleUrls: ['./popular-movies.component.css']
 })
 export class PopularMoviesComponent implements OnInit {
-  constructor(private popularSvc: PopularMoviesApiService) { }
-  popular: Observable<any>;  
-  ngOnInit() {
-    this.getPopular();
-  } 
+  PUBLIC_KEY = '0268e9667082b43101efe70dc24d137b';
+  URL_API = `https://api.themoviedb.org/3/movie/popular?api_key=${this.PUBLIC_KEY}&language=pt-BR&page=1`;
+  MOVIES
 
-  getPopular() {
-    this.popular = this.popularSvc.getPopular();
+  constructor(public popularSvc: PopularMoviesApiService) { }
+  ngOnInit() {
+     fetch(this.URL_API)
+    .then(res => res.json()).then(data => {
+      console.log(data.results)
+      data.results.map(data => {
+        this.MOVIES = {
+          name: data.title,
+          date: data.release_date,
+          image: data.backdrop_path,
+        }  
+      })
+    })
   }
+
 }
